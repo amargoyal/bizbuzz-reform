2eeb9416f46620501787a460d1fa08212f3916af
amar <goyalamars18@gmail.com>
2026-09-15 19:00:39 -0500

    fix program shortcut

--- bizbuzz/src/components/about/AboutIntro.tsx
+++ bizbuzz/src/components/about/AboutIntro.tsx
@@ -9,7 +9,7 @@ export default function AboutIntro() {
         <p className="bb-lead">Allen Xu and Aarav Khullar founded BizBuzz in April 2024 as juniors at Naperville North High School.</p>
         <p className="bb-body">After finding that 90% of local K–8 schools offered no business education, they created a place for younger students to develop an idea and the confidence to share it.</p>
         <p className="bb-body">Today, BizBuzz NFP is a 501(c)(3) nonprofit serving Chicagoland through free camps, workshops and pitch competitions. Students design the programs, teach the sessions and mentor the next group of young entrepreneurs.</p>
-        <ArrowCTA href="/camps">Explore our programs</ArrowCTA>
+        <ArrowCTA href="/#programs">Explore our programs</ArrowCTA>
       </div>
       <figure className="bb-about-team-photo">
         <div><Image src="/about/background.jpg" alt="The BizBuzz team and students together" fill priority sizes="(max-width: 900px) 100vw, 45vw" /></div>
